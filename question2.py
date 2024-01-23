c70d6b788ffe815da8278ce597993aea6918da4f
prymnii <69064767+prymnii@users.noreply.github.com>
2024-01-22 21:00:12 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,15 +1,20 @@
 def question2_1():
     # answer = ['string', 'string', 'string']
+    answer = ['quantitative','ratio','N/A']
     return answer
 
 def question2_2():
+    answer = ['quantitative','interval or ratio','answer depends on if you count reversing as negative speed']
     return answer
 
 def question2_3():
+    answer = ['quantitative','ratio','measured as a ratio to the threshold of human hearing']
     return answer 
 
 def question2_4():
+    answer = ['qualitative','ordinal','N/A' ]
     return answer
 
 def question2_5():
+    answer = ['qualitative', 'nominal', 'N/A']
     return answer
