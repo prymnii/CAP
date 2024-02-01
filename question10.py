91372f725d2c5cd52f977bbad167b7afefffbec7
prymnii <69064767+prymnii@users.noreply.github.com>
2024-01-31 21:12:09 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,32 +1,44 @@
 def question10_1():
-    """
-    answer: python bool (True or False)
-    """
+    answer = bool (False)
+    # noise can affect the reliability, interpretation, and accuracy of count data
     return answer
 
 def question10_2():
+    answer = bool (True)
+    # -1 = maximum negative relationship, 0 = no relationship, 1 = maximum positive relationship
     return answer
 
 def question10_3():
+    answer = bool (False)
+    # sampling misses fluctuations and anomalies. This is especially ineffecient for a time series, where these are common. 
     return answer
 
 def question10_4():
+    answer = bool (True)
+    # They can be the same in the sense that they can be interpreted as either or. Definely, they are fundamentally different.
     return answer
 
 def question10_5():
+    answer = bool (False)
     return answer
 
 def question10_6():
+    answer = bool (False)
     return answer
 
 def question10_7():
+    answer = bool (False)
+    # indicates similar direction, not "identicality"
     return answer
 
 def question10_8():
+    answer = bool (False)
     return answer
 
 def question10_9():
+    answer = bool (False)
     return answer
 
 def question10_10():
+    answer = bool (False)
     return answer
